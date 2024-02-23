a5e5b9976393ab3684fbb9a39c490055d3242630
Erum Sikander <mrsikanderghouri@gmail.com>
2024-02-24 01:07:13 +0500

    Add files via upload

---
+++ new.ts
@@ -0,0 +1,10 @@
+
+// How to change color 1st value to 2nd value ?
+
+// 1:
+ let favouriteColor = "Black";
+console.log(favouriteColor);
+// 2:
+favouriteColor = "White";
+console.log(favouriteColor);
+
